210dc112104634b305959277389604da28233514
Steve <steve21johnson@gmail.com>
2023-02-07 15:52:39 -0800

    rm footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, {ReactNode} from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 
@@ -7,12 +7,12 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({children, title = 'This is the default title'}: Props) => (
   <div>
     <Head>
       <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
     </Head>
     <header>
       <nav>
@@ -23,8 +23,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     </header>
     {children}
     <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+      <hr/>
     </footer>
   </div>
 )

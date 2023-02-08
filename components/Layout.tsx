941e04b7dca52b03860e4e6e1fffb661e4dcb173
Steve <steve21johnson@gmail.com>
2023-02-07 18:41:06 -0800

    rm header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,13 +14,6 @@ const Layout = ({children, title = 'This is the default title'}: Props) => (
       <meta charSet="utf-8"/>
       <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
     </Head>
-    <header>
-      <nav>
-        <Link href="/">Home</Link> | <Link href="/about">About</Link> |{' '}
-        <Link href="/users">Users List</Link> |{' '}
-        <a href="/api/users">Users API</a>
-      </nav>
-    </header>
     {children}
     <footer>
       <hr/>

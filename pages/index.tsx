116c65e4a07b59ff81c0bef5e779422470f51c2b
Steve <steve21johnson@gmail.com>
2023-02-07 16:23:35 -0800

    fetcher

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import useSWR from 'swr'
 
-const IndexPage = () => (
-  <Layout title="Chat App">
-    <h1>Chat App</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+const fetcher = (...args: [input: RequestInfo | URL, init?: RequestInit | undefined]) => fetch(...args).then((res) => res.json())
+
+const IndexPage = () => {
+  const {data, error} = useSWR('/api/users', fetcher)
+  console.log('data', data)
+  return (
+    <Layout title="Chat App">
+      <h1>Chat App</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage

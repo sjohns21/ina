9cdc286f972060d5dded168f54b6a00e32d5f645
Steve <steve21johnson@gmail.com>
2023-02-28 13:53:54 -0800

    loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Layout from "../components/Layout";
 import { useMemo, useState } from "react";
-import { IconButton, Input } from "@mui/material";
+import { CircularProgress, IconButton, Input } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 
 const fetcher = (
@@ -10,13 +10,16 @@ const fetcher = (
 const IndexPage = () => {
   const [thread, setThread] = useState(initialThread);
   const [addition, setAddition] = useState("");
+  const [loading, setLoading] = useState(false);
   const rows = useMemo(() => thread.split("\n").slice(2), [thread]);
 
   const send = async () => {
     let prompt = thread + addition + "\nAI:";
+    setAddition("");
+    setLoading(true);
     let response = await fetcher(`/api/chat?prompt=${prompt}`);
     setThread(prompt + response + "\nHuman: ");
-    setAddition("");
+    setLoading(false);
   };
   return (
     <Layout title="Chat Plus">
@@ -25,16 +28,22 @@ const IndexPage = () => {
           {row}
         </div>
       ))}
-      <div style={{ display: "flex" }}>
-        <Input
-          value={addition}
-          onChange={(e) => setAddition(e.target.value)}
-          fullWidth
-          onKeyDown={(e) => e.key === "Enter" && send()}
-        />
-        <IconButton aria-label="send" onClick={send}>
-          <SendIcon />
-        </IconButton>
+      <div style={{ display: "flex", height: "1.5em" }}>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <Input
+              value={addition}
+              onChange={(e) => setAddition(e.target.value)}
+              fullWidth
+              onKeyDown={(e) => e.key === "Enter" && send()}
+            />
+            <IconButton aria-label="send" onClick={send}>
+              <SendIcon />
+            </IconButton>
+          </>
+        )}
       </div>
     </Layout>
   );

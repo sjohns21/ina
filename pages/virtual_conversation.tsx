42842226a8cc492434c84a40be56cd74135599c8
Steve <steve21johnson@gmail.com>
2023-03-01 10:45:57 -0800

    virtual conversation

---
+++ pages/virtual_conversation.tsx
@@ -0,0 +1,30 @@
+import Layout from "@/components/Layout";
+import { Button, Input } from "@mui/material";
+import { useState } from "react";
+import { Chat } from "@/components/chat";
+
+type Props = {};
+const VirtualConversationPage = (props: Props) => {
+  const [character, setCharacter] = useState("");
+  const [characterSet, setCharacterSet] = useState(false);
+  return (
+    <Layout title="Virtual Conversation">
+      Have a virtual conversation with an AI copy of: -a famous person -an
+      author -a fictional character
+      <Input value={character} onChange={(e) => setCharacter(e.target.value)} />
+      {!characterSet ? (
+        <Button variant="text" onClick={() => setCharacterSet(true)}>
+          Start
+        </Button>
+      ) : (
+        <Chat
+          prompt={`The following is a conversation with an AI copy of ${character}. The AI is helpful, creative, clever, and very friendly.
+
+Human: `}
+        />
+      )}
+    </Layout>
+  );
+};
+
+export default VirtualConversationPage;

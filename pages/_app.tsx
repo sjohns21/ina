33bbd2f20d617c9bf935547987ad7f14c59f006c
Steve <steve21johnson@gmail.com>
2023-03-02 14:12:34 -0800

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from "next/app";
 import { CssBaseline } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <CssBaseline />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }

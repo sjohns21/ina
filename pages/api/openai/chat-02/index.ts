9ad6060f4197fb72ffdc41eb4ac0788b78c8ac7a
Steve <steve21johnson@gmail.com>
2023-03-04 14:34:59 -0800

    api/openai

--- pages/api/openai/chat-02/index.ts
+++ pages/api/openai/chat-02/index.ts
@@ -1,10 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Configuration, OpenAIApi } from "openai";
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+import { openai } from "@/pages/api/openai";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await openai.createCompletion({

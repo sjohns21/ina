50f94cd2e200a1bb8408280be1cc0e71330890b6
Steve <steve21johnson@gmail.com>
2023-03-08 13:58:18 -0800

    new chat api

---
+++ pages/api/openai/chat/index.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { openai } from "@/pages/api/openai";
+import { ChatCompletionRequestMessage } from "openai";
+
+type Body = {
+  body: {
+    messages: ChatCompletionRequestMessage[];
+  };
+};
+const handler = async (req: NextApiRequest & Body, res: NextApiResponse) => {
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: req.body.messages,
+  });
+  res.status(200).json(response.data.choices[0].message);
+};
+
+export default handler;

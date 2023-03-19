41037d5b521afb8de2ee5f323169c041dd85cb5f
Steve <steve21johnson@gmail.com>
2023-03-18 20:40:45 -0700

    chat gpt clone page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,3 @@
-import { Chat02 } from "@/components/chat-02";
+const IndexPage = () => "index";
 
-const IndexPage = () => <Chat02 prompt={defaultPrompt} AILabel={"AI"} />;
-
-const defaultPrompt = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
-
-You: `;
 export default IndexPage;

8ca60383b3607dcb8acac91fddad7b3eb5c47b9b
Steve <steve21johnson@gmail.com>
2023-03-21 12:27:12 -0700

    summarize

---
+++ pages/api/openai/summarize.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { openai } from "@/pages/api/openai/index";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `Convert my short hand into a first-hand account of the meeting:\n\n${req.query.text}`,
+    temperature: 0,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0
+  });
+  res.status(200).json(response.data.choices[0].text);
+};
+
+export default handler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,21 @@ import { useState } from "react";
 
 const IndexPage = () => {
   const [transcript, setTranscript] = useState("");
+  const [summary, setSummary] = useState("");
+  const getSummary = async () => {
+    setSummary(await (await fetch("/api/openai/summarize?text=" + transcript)).text());
+  };
   return <div className={"flex h-full"}>
     <div style={{ background: "#E7E9FF" }}>left</div>
     <div>center
       <br />
       {transcript}
     </div>
-    <div>right</div>
+    <div>right
+      <div>{summary}</div>
+    </div>
     <RecordAudio setTranscript={setTranscript} />
+    <button onClick={getSummary}>Get summary</button>
   </div>;
 };
 

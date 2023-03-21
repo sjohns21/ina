f4b23fa743a6e47457eeb364ba108bcc236feb05
Steve <steve21johnson@gmail.com>
2023-03-21 14:21:38 -0700

    summarize in bullets

--- pages/api/openai/summarize.ts
+++ pages/api/openai/summarize.ts
@@ -4,12 +4,12 @@ import { openai } from "@/pages/api/openai/index";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `The following is a dialogue between a doctor and patient. Convert it into a summary:\n\n${req.query.text}`,
+    prompt: `The following is a dialogue between a doctor and patient. Convert it into a summary in bullet points:\n\n${req.query.text}`,
     temperature: 0,
     max_tokens: 256,
     top_p: 1,
     frequency_penalty: 0,
-    presence_penalty: 0
+    presence_penalty: 0,
   });
   res.send(response.data.choices[0].text);
 };

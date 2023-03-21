0462c1b0a00b69faaea5ba57a7c760fd99b95e90
Steve <steve21johnson@gmail.com>
2023-03-21 12:49:47 -0700

    re prompt

--- pages/api/openai/summarize.ts
+++ pages/api/openai/summarize.ts
@@ -4,7 +4,7 @@ import { openai } from "@/pages/api/openai/index";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `Convert my short hand into a first-hand account of the meeting:\n\n${req.query.text}`,
+    prompt: `The following is a dialogue between a doctor and patient. Convert it into a summary:\n\n${req.query.text}`,
     temperature: 0,
     max_tokens: 256,
     top_p: 1,

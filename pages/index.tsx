a4c1ba7d187e47d9e07046b47c6c96879d2b6175
Steve <steve21johnson@gmail.com>
2023-03-20 20:05:39 -0700

    3 cols

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 
 const IndexPage = () => {
   const [transcript, setTranscript] = useState("");
-  return <div>
+  return <div className={"flex h-full"}>
+    <div style={{ background: "#E7E9FF" }}>left</div>
+    <div>center
+      <br />
+      {transcript}
+    </div>
+    <div>right</div>
     <RecordAudio setTranscript={setTranscript} />
-    {transcript}
   </div>;
 };
 

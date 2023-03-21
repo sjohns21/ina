3d95d3fe1ac21c9e529fccd07df0deb1f24d5a17
Steve <steve21johnson@gmail.com>
2023-03-20 18:23:09 -0700

    tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import type { AppProps } from "next/app";
 import { CssBaseline } from "@mui/material";
 import { Analytics } from "@vercel/analytics/react";
+import "../globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App ({ Component, pageProps }: AppProps) {
   return (
     <>
       <CssBaseline />

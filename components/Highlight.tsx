f55c2fdc5a18bc68c43f401aaeb09863cac5fb24
Steve <steve21johnson@gmail.com>
2023-03-22 16:18:21 -0700

    Highlight

---
+++ components/Highlight.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+
+export default function Highlight({
+  label,
+  content,
+}: {
+  label: string;
+  content: string;
+}) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <List>
+      <ListItemButton onClick={handleClick}>
+        <ListItemText primary={label} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {content}
+        </List>
+      </Collapse>
+    </List>
+  );
+}

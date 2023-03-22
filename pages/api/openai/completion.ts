4fe671de4f63188d065431daef8eea641ff0f4d4
Steve <steve21johnson@gmail.com>
2023-03-22 15:24:33 -0700

    completion api

---
+++ pages/api/openai/completion.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { openai } from "@/pages/api/openai/index";
+import { CreateCompletionRequest } from "openai";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const obj = { ...defaultObj, ...req.query };
+  const response = await openai.createCompletion(obj);
+  res.send(response.data.choices[0].text);
+};
+const defaultObj: CreateCompletionRequest = {
+  model: "text-davinci-003",
+  prompt: "",
+  temperature: 0,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+};
+export default handler;

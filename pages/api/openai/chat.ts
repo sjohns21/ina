37c53f957f14018bebdd01fd857b906687ace21e
Steve <steve21johnson@gmail.com>
2023-04-14 16:43:41 -0700

    3.5-turbo

--- pages/api/openai/chat.ts
+++ pages/api/openai/chat.ts
@@ -9,7 +9,7 @@ type Body = {
 };
 const handler = async (req: NextApiRequest & Body, res: NextApiResponse) => {
   const response = await openai.createChatCompletion({
-    model: "gpt-4",
+    model: "gpt-3.5-turbo",
     messages: req.body.messages,
   });
   res.status(200).json(response.data.choices[0].message);

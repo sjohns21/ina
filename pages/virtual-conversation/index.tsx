856f2b0139db19e1adc074082f79e9d535995bdd
Steve <steve21johnson@gmail.com>
2023-05-15 13:21:11 -0400

    Revert "posthog capture start virtual conversation with character"
    This reverts commit c871d20188ad4384d48f95df85ae4386e7d6ffde.

--- pages/virtual-conversation/index.tsx
+++ pages/virtual-conversation/index.tsx
@@ -2,11 +2,9 @@ import Layout from "@/components/Layout";
 import { Button, Input } from "@mui/material";
 import { useState } from "react";
 import { useRouter } from "next/router";
-import { usePostHog } from "posthog-js/react";
 
 const VirtualConversationPage = () => {
   const router = useRouter();
-  const posthog = usePostHog();
   const [character, setCharacter] = useState("");
 
   return (
@@ -20,12 +18,11 @@ const VirtualConversationPage = () => {
       {character && (
         <Button
           variant="text"
-          onClick={() => {
-            posthog?.capture("start virtual conversation", { character });
+          onClick={() =>
             router.push(
               "/virtual-conversation/" + character.replaceAll(" ", "-")
-            );
-          }}
+            )
+          }
         >
           Start
         </Button>

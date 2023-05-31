2586bc7f376f00205204da04cf32494cab7145f8
Steve <steve21johnson@gmail.com>
2023-05-31 10:51:30 -0700

    index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,20 @@
+import Layout from "@/components/Layout";
+
 const Index = () => {
-  return <div>Index</div>;
+  return (
+    <Layout title="ina">
+      <div className="flex items-center justify-center">
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2Y1N2U5ZmEzZDUxNjRlMjhjM2E3NjMwM2Q2OGVjOTY3YTIxZmJjMCZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/p6RVGDmsXrONHCYG3R/giphy.gif"
+          alt="Water Refreshing GIF by xponentialdesign"
+          className="w-1/2"
+        />
+        <div className="w-1/2 text-center">
+          <h1 className="text-4xl font-bold">Tools for Entrepreneurs</h1>
+        </div>
+      </div>
+    </Layout>
+  );
 };
+
 export default Index;

b6258b1097b267ff66fa3de76c52605fcf37ba1c
Steve <steve21johnson@gmail.com>
2023-05-31 12:13:01 -0700

    feedback analyzer marketing

---
+++ pages/feedback-analyzer/marketing.tsx
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+type Props = {};
+
+export default function Marketing({ }: Props) {
+    return (
+        <Layout title="Feedback Analyzer">
+            <div>marketing</div>
+        </Layout>
+    );
+}

a6fa568ebedfaf1f89add20dd7ef66974e0caed5
Steve <steve21johnson@gmail.com>
2023-05-31 13:49:18 -0700

    icons

--- pages/feedback-analyzer/marketing.tsx
+++ pages/feedback-analyzer/marketing.tsx
@@ -1,6 +1,10 @@
 import Layout from '@/components/Layout';
 import React from 'react';
 import ArrowIcon from '@mui/icons-material/ArrowCircleRight';
+import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import BugReportIcon from '@mui/icons-material/BugReport';
+import FeaturedVideoIcon from '@mui/icons-material/FeaturedVideo';
 type Props = {};
 
 export default function Marketing({ }: Props) {
@@ -10,41 +14,32 @@ export default function Marketing({ }: Props) {
             <h2>Analyze user feedback, understand your customers, improve your product</h2>
             <div className='flex justify-between'>
                 <section className='w-1/4'>
-                    <h3>Raw Feedback
-                    </h3>
+                    <RecordVoiceOverIcon className='w-full' />
+                    <h3>Raw Feedback</h3>
                     <p>It would be nice if the documentation included search</p>
                     <p>Where is `Getting Started` in the documenation?</p>
                     <p>Haha</p>
                 </section>
                 <ArrowIcon className='m-2 mt-5' />
                 <section className='w-1/4'>
+                    <FilterAltIcon className='w-full' />
                     <h3>Filtered Feedback</h3>
                     <p>It would be nice if the documentation included search</p>
                     <p>Where is `Getting Started` in the documenation?</p>
                 </section>
                 <ArrowIcon className='m-2 mt-5' />
                 <section className='w-1/4'>
+                    <BugReportIcon className='w-full' />
                     <h3>Identified Problems</h3>
                     <p>The documentation is hard to navigate</p>
                 </section>
                 <ArrowIcon className='m-2 mt-5' />
                 <section className='w-1/4'>
+                    <FeaturedVideoIcon className='w-full' />
                     <h3>Product Suggestions</h3>
                     <p>Add search and navigation to the documentation</p>
                 </section>
             </div>
-            <style jsx>{`
-                .feedback-container {
-                        border: 1px solid #ddd;
-                    padding: 1rem;
-                    margin-bottom: 1rem;
-                }
-
-                    .feedback-container p {
-                        font - size: 0.8rem;
-                    margin: 0; 
-                }`}
-            </style>
         </Layout>
     );
 }

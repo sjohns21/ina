de6469871db388f7cb4bc1b0cf523b475e3f3385
Steve <steve21johnson@gmail.com>
2023-05-31 10:59:48 -0700

    round

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
         <img
           src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExM2Y1N2U5ZmEzZDUxNjRlMjhjM2E3NjMwM2Q2OGVjOTY3YTIxZmJjMCZlcD12MV9pbnRlcm5hbF9naWZzX2dpZklkJmN0PWc/p6RVGDmsXrONHCYG3R/giphy.gif"
           alt="Water Refreshing GIF by xponentialdesign"
-          className="w-1/2"
+          className="w-1/2 rounded-lg"
         />
         <div className="w-1/2 text-center">
           <h1 className="text-4xl font-bold">Tools for Entrepreneurs</h1>

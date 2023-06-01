a29a4c7c243db81b8e2737b9718cac1b6740a571
Steve <steve21johnson@gmail.com>
2023-05-31 17:15:20 -0700

    supabase client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
 import "../globals.css";
 import { CssBaseline } from "@mui/material";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import Script from "next/script";
 import { DevSupport } from "@react-buddy/ide-toolbox-next";
 import { ComponentPreviews, useInitial } from "@/components/dev";
+import { Session, createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== "undefined") {
@@ -25,8 +27,11 @@ if (typeof window !== "undefined") {
   });
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps<{
+  initialSession: Session,
+}>) {
   const router = useRouter();
+  const [supabase] = useState(() => createPagesBrowserClient({ supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL, supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY }));
   useEffect(() => {
     // Track page views
     const handleRouteChange = () => posthog?.capture("$pageview");
@@ -38,18 +43,19 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.events]);
   return (
     <>
-      <CssBaseline />
-      <PostHogProvider client={posthog}>
-        <DevSupport
-          ComponentPreviews={ComponentPreviews}
-          useInitialHook={useInitial}
-        >
-          <Component {...pageProps} />
-        </DevSupport>
-      </PostHogProvider>
-      <Analytics />
-      <Script id={"hotjar"}>
-        {`(function(h,o,t,j,a,r){
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <CssBaseline />
+        <PostHogProvider client={posthog}>
+          <DevSupport
+            ComponentPreviews={ComponentPreviews}
+            useInitialHook={useInitial}
+          >
+            <Component {...pageProps} />
+          </DevSupport>
+        </PostHogProvider>
+        <Analytics />
+        <Script id={"hotjar"}>
+          {`(function(h,o,t,j,a,r){
           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
           h._hjSettings={hjid:3484792,hjsv:6};
           a=o.getElementsByTagName('head')[0];
@@ -57,7 +63,8 @@ export default function App({ Component, pageProps }: AppProps) {
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
           a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-      </Script>
+        </Script>
+      </SessionContextProvider>
     </>
   );
 }
